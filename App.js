37c588a0df7ffa54cabf5d21db965ad4bcef748b
Vivek Vardhan <91594529+vivekvardhan2810@users.noreply.github.com>
2024-09-25 13:20:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import VideoPlayer from './VideoPlayer';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Custom Video Player</h1>
+      <VideoPlayer />
+    </div>
+  );
+}
+
+export default App;
+
